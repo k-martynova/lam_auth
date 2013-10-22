ffdbc6659add4096e59d6f4f62ea265025e57f60
Max Savchenko <robotector@gmail.com>
2013-10-22 09:21:21 -0700

    dont use assets for village

--- lib/lam_auth.rb
+++ lib/lam_auth.rb
@@ -14,16 +14,16 @@ module LamAuth
     def config
       {
         'lookatme' => {
-          'prefix' => 'lam', 
+          'prefix' => 'lam',
           'url' => 'http://www.lookatme.ru',
           'api' => 'LAM',
           'api_url' => 'http://www.lookatme.ru/api/js/LAM.Login.js',
         },
         'village' => {
-          'prefix' => 'village', 
+          'prefix' => 'village',
           'url' => 'http://www.the-village.ru',
           'api' => 'VLG',
-          'api_url' => 'http://assets0.the-village.ru/api/js/VLG.Login.js',
+          'api_url' => 'http://www.the-village.ru/api/js/VLG.Login.js',
         }
       }[site]
     end
@@ -31,11 +31,11 @@ module LamAuth
     def site
       %w{lookatme village}.include?(app['site']) ? app['site'] : 'lookatme'
     end
-    
+
     def url
       config['url']
     end
-    
+
     def cookie_id
       "#{config['prefix']}_#{app['app_id']}"
     end
@@ -44,11 +44,11 @@ module LamAuth
       hash = parse_cookie_to_hash(cookie)
       valid_hash?(hash) && hash['access_token']
     end
-    
+
     def parse_cookie_to_hash(data)
       Rack::Utils.parse_query(Rack::Utils.unescape(data.to_s.gsub('"', '')))
     end
-    
+
     def valid_hash?(hash)
       Digest::MD5.hexdigest(hash.except('sig').sort.map{|v| v.join('=')}.join + app['secret']) == hash['sig']
     end
